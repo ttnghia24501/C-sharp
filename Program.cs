c39a63aa220856c74ceb59644895607b41617c24
ttnghia24501 <53210685+ttnghia24501@users.noreply.github.com>
2020-04-17 00:18:35 +0700

    lap1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Nhap vao so nguyen n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.ReadKey();
+        }
+
+        static void Fi(int n)
+        {
+            int fb = 0, fb1= 1,fb2 = 1;
+            
+            while (fb1+fb2 < n )
+            {
+                fb = fb1 + fb2;
+                fb1 = fb2;
+                fb2 = fb;
+            }
+            Console.Write("so can tim la : {0} ",fb);
+        }
+    }
+}
